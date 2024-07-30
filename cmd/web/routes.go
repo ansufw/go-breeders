0638deb0e84afccec6926c797f3066b31b433759
Ananto Y.W <ananto@geniox.com>
2024-07-30 06:59:21 +0200

    render first page

---
+++ cmd/web/routes.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// recover from panics, send back 500, keep the application running
+	mux.Use(middleware.Recoverer)
+
+	// any request that takes more than 60s will timeout
+	mux.Use(middleware.Timeout(60 * time.Second))
+
+	mux.Get("/", app.ShowHome)
+
+	return mux
+}
